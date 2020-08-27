73ddd3401a2921b04bb8be83b7a11bc924138340
berlybeer <b.pumacajia.cz@gmail.com>
2020-08-26 21:29:28 -0500

    Solve javascript error in vuejs

--- src/main.js
+++ src/main.js
@@ -5,6 +5,18 @@ import router from "./router";
 Vue.config.productionTip = false;
 
 Vue.component("alert", require('./components/Alert.vue').default);
+// Vue.component("boton", require('./components/Button.vue').default);
+
+// Define a new component called s
+
+Vue.component('button-counter', {
+	data: function(){
+		return{
+			count: 0
+		}
+	},
+	template: '<button v-on:click="count++">You clicked me {{count}} times.</button>'
+})
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="Welcome to Your Vue2  App" />
+	<div id="components-demo">
+	  <button-counter></button-counter>
+	</div>
   </div>
 </template>
 
