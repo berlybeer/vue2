2d6d6eb56c8ff0d22540eb553194f24b8fbb9521
berlybeer <b.pumacajia.cz@gmail.com>
2020-08-28 17:50:13 -0500

    Delete item from array list

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -5,7 +5,9 @@
     <input @keyup.enter="add" type="text" v-model="title" placeholder="What you want to do" />
     <button @click="add">Add to list</button>
 	<ul>
-		<li v-for="todo in todos" v-bind:key="todo">{{todo}}</li>
+		<li v-for="(todo,index) in todos" v-bind:key="todo">{{todo}}
+			<span @click="remove(index)" class="remove">x</span>
+		</li>
 	</ul>
   </div>
 </template>
@@ -26,12 +28,34 @@ export default {
 	},
 	methods: {
 		add(){
-			this.todos.push(this.title);
-			this.title = "";
+			if(this.title != ''){
+				
+				this.todos.unshift(this.title);
+				this.title = "";
+			}
+		},
+		remove(id){
+			// console.log(id);
+			// this.todos.splice(id,1)
+			this.$delete(this.todos, id);
 		}
 	}
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+	.remove{
+		padding: .3rem .6rem;
+		background-color: red;
+		color: #fff;
+		border-radius: 30px;
+	}
+	.remove:hover{
+		cursor:pointer;
+	}
+
+	ul li{
+		padding: .4rem;
+	}
+</style>
